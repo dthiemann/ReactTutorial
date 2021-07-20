8b4ddc438e911d2e0f285586b1ef8de51ef5c9f9
dthiemann <dylanthiemann@gmail.com>
2021-07-19 21:32:24 -0500

    Setting a blank slate for testing - old code saved but commented out for quick reference

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,76 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
 
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.setState({ value: "X" })}>
-                {this.props.value}
-            </button>
-        );
-    }
-}
-
-class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />;
-    }
-
-    render() {
-        const status = "Next player: X";
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
 
 // ========================================
 
 ReactDOM.render(
-    <Game />, document.getElementById("root"));
+    <App />, document.getElementById("root"));
 
 function playingWithArrays() {
     const companies = [
@@ -189,4 +125,70 @@ function playingWithArrays() {
 
 }
 
-playingWithArrays();
+// playingWithArrays();
+
+// class Square extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             value: null,
+//         };
+//     }
+
+//     render() {
+//         return (
+//             <button
+//                 className="square"
+//                 onClick={() => this.setState({ value: "X" })}>
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+// class Board extends React.Component {
+//     renderSquare(i) {
+//         return <Square value={i} />;
+//     }
+
+//     render() {
+//         const status = "Next player: X";
+
+//         return (
+//             <div>
+//                 <div className="status">{status}</div>
+//                 <div className="board-row">
+//                     {this.renderSquare(0)}
+//                     {this.renderSquare(1)}
+//                     {this.renderSquare(2)}
+//                 </div>
+//                 <div className="board-row">
+//                     {this.renderSquare(3)}
+//                     {this.renderSquare(4)}
+//                     {this.renderSquare(5)}
+//                 </div>
+//                 <div className="board-row">
+//                     {this.renderSquare(6)}
+//                     {this.renderSquare(7)}
+//                     {this.renderSquare(8)}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// class Game extends React.Component {
+//     render() {
+//         return (
+//             <div className="game">
+//                 <div className="game-board">
+//                     <Board />
+//                 </div>
+//                 <div className="game-info">
+//                     <div>{/* status */}</div>
+//                     <ol>{/* TODO */}</ol>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
